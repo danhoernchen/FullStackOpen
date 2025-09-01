bdd24f94826b7e2285a392e9ae4e1b161ff249af
Daniel <191582347+danhoernchen@users.noreply.github.com>
2025-09-01 09:06:39 +0200

    Exercises 4.13 and 4.14

--- Part4/bloglist/controllers/blog.js
+++ Part4/bloglist/controllers/blog.js
@@ -9,7 +9,7 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.get('/:id', async (req, res) => {
   const note = await Blog.findById(req.params.id)
   if (note) {
-    res.send(note)
+    res.json(note)
   } else {
     res.status(404).send('message id not in db')
   }
@@ -22,6 +22,27 @@ blogRouter.post('/', async (request, response) => {
     response.status(201).json(saved)
   } catch (error) {
     response.status(400).send(error)
+  }
+})
+
+blogRouter.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Blog.findByIdAndDelete(req.params.id)
+    res.status(204).json(deleted)
+  } catch (error) {
+    res.status(404).json({ error })
+  }
+})
+
+blogRouter.put('/:id', async (req, res) => {
+  try {
+    const post = await Blog.findById(req.params.id)
+    post.likes = req.body.likes
+    const updated = await post.save()
+    res.status(201).json(updated)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error)
   }
 })
 
